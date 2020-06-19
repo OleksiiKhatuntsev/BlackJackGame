ace5406e70b14842b2a7aa6a79fb0b2b788b1e65
OleksiiKhatuntsev <kaelthasmafia@outlook.com>
2020-06-19 11:15:37 +0300

    add bets

--- game.py
+++ game.py
@@ -174,30 +174,46 @@ def fill_deck(deck):
 if __name__ == "__main__":
     deck = fill_deck([])
     random.shuffle(deck)
-    for card in deck:
-        print(card)
     name = input("Provide your name: ")
     value = int(input("Provide your balance: "))
     player = Player(name, value)
     dealer = Player("dealer", 100500)
-    dealer_hand = Hand(dealer)
-    player_hand = Hand(player)
-
     while True:
-        if check_hand(player_hand) <= 21:
-            print(player_hand)
-            player_definition = input("Do you wanna another card? (y/n)")
-            if player_definition == 'y':
-                create_turn(player_hand)
-                print(dealer_hand)
-                print(player_hand)
-            elif player_definition == 'n':
-                print(finish_game(True, check_hand(player_hand), dealer_hand))
-                break
-            else:
-                continue
-        else:
-            print(finish_game(False, check_hand(player_hand), dealer_hand))
-            print(dealer_hand)
-            print(player_hand)
+        result = ''
+        bet= ''
+        if player.balance <= 0:
+            print("You lose")
             break
+        else:
+            bet = int(input("Make your bet"))
+            if(player.balance - bet < 0):
+                print("You haven't enough money")
+                continue
+            dealer_hand = Hand(dealer)
+            player_hand = Hand(player)
+
+            while True:
+                if check_hand(player_hand) <= 21:
+                    print(player_hand)
+                    player_definition = input("Do you wanna another card? (y/n)")
+                    if player_definition == 'y':
+                        create_turn(player_hand)
+                        print(dealer_hand)
+                        print(player_hand)
+                    elif player_definition == 'n':
+                        result = finish_game(True, check_hand(player_hand), dealer_hand)
+                        break
+                    else:
+                        continue
+                else:
+                    result = finish_game(False, check_hand(player_hand), dealer_hand)
+                    print(dealer_hand)
+                    print(player_hand)
+                    break
+        if result == WinStatus.WIN:
+            player.balance += bet
+        elif result == WinStatus.LOSE:
+            player.balance -= bet
+
+        print(result)
+        print(player.balance)
